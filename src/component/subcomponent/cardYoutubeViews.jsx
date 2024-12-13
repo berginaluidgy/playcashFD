020adc04438440fb833a664fec7af0f1e47c5071
berginaluidgy <156596145+berginaluidgy@users.noreply.github.com>
2024-12-13 00:05:25 -0500

    Update cardYoutubeViews.jsx

--- src/component/subcomponent/cardYoutubeViews.jsx
+++ src/component/subcomponent/cardYoutubeViews.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Confetti from "react-confetti";
-import "./CardYoutubeViews.css";
+import "./cardYoutubeViews.css";
 import axios from "axios";
 import acsHOMEINFO from '../Account/access/acsHOMEINFO'
 import Authconfirmator from "../Auth.confirmator";
