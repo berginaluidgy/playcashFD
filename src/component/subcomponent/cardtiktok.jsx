9c53d8109d0bf60457fd5e0f57bc309197f91129
berginaluidgy <luidgybergina7@gmail.com>
2024-12-12 21:54:37 -0500

    first2b

--- src/component/subcomponent/cardtiktok.jsx
+++ src/component/subcomponent/cardtiktok.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import './Cardtiktok.css'
-// import CardtiktokFOll from './cardtiktokFOll'
-import VideoManager from '../videosCreator/vdeoscreator'
-export default function Cardtiktok() {
-  return (
-    <div id='cardtiktok'>
+// import React from 'react'
+// import './Cardtiktok.css'
+// // import CardtiktokFOll from './cardtiktokFOll'
+// import VideoManager from '../videosCreator/vdeoscreator'
+// export default function Cardtiktok() {
+//   return (
+//     <div id='cardtiktok'>
 
-<VideoManager/>
-    </div>
-  )
-}
+// <VideoManager/>
+//     </div>
+//   )
+// }
